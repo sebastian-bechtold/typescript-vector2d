7e82695bf2db5901ee44826e07cb45fc6408c17f
sbecht <sbecht>
2018-03-18 09:29:07 +0100

    - Added more vector methods.

--- src/Vec2d.js
+++ src/Vec2d.js
@@ -1,29 +1,40 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var Vec2d = (function () {
-        function Vec2d(x, y) {
-            this.x = 0;
-            this.y = 0;
-            this.x = x;
-            this.y = y;
-        }
-        Vec2d.prototype.add = function (v) {
-            return new Vec2d(this.x + v.x, this.y + v.y);
-        };
-        Vec2d.prototype.copy = function () {
-            return new Vec2d(this.x, this.y);
-        };
-        Vec2d.prototype.mult = function (scalar) {
-            return new Vec2d(this.x * scalar, this.y * scalar);
-        };
-        Vec2d.prototype.subtract = function (v) {
-            return new Vec2d(this.x - v.x, this.y - v.y);
-        };
-        Vec2d.prototype.toString = function () {
-            return "(" + this.x + ", " + this.y + ")";
-        };
-        return Vec2d;
-    }());
-    exports.Vec2d = Vec2d;
-});
+"use strict";
+exports.__esModule = true;
+var Vec2d = /** @class */ (function () {
+    function Vec2d(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.x = x;
+        this.y = y;
+    }
+    Vec2d.prototype.add = function (v) {
+        return new Vec2d(this.x + v.x, this.y + v.y);
+    };
+    Vec2d.prototype.copy = function () {
+        return new Vec2d(this.x, this.y);
+    };
+    Vec2d.prototype.distance = function (v) {
+        return (v.subtract(this)).norm();
+    };
+    Vec2d.prototype.mult = function (scalar) {
+        return new Vec2d(this.x * scalar, this.y * scalar);
+    };
+    Vec2d.prototype.norm = function () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    };
+    Vec2d.prototype.normalize = function () {
+        var norm = this.norm();
+        return new Vec2d(this.x / norm, this.y / norm);
+    };
+    Vec2d.prototype.subtract = function (v) {
+        return new Vec2d(this.x - v.x, this.y - v.y);
+    };
+    Vec2d.prototype.scalarMult = function (a) {
+        return new Vec2d(this.x * a, this.y * a);
+    };
+    Vec2d.prototype.toString = function () {
+        return "(" + this.x + ", " + this.y + ")";
+    };
+    return Vec2d;
+}());
+exports.Vec2d = Vec2d;
--- src/Vec2d.ts
+++ src/Vec2d.ts
@@ -18,15 +18,33 @@ export class Vec2d {
     }
     
     
+    distance(v : Vec2d) {
+        return (v.subtract(this)).norm();
+    }
+    
     mult(scalar : number) {
         return new Vec2d(this.x * scalar, this.y * scalar);
     }
     
+    norm() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    
+    normalize() : Vec2d {
+        let norm = this.norm();
+
+        return new Vec2d(this.x / norm, this.y / norm);
+    }
+
     subtract(v : Vec2d) : Vec2d {        
         return new Vec2d(this.x - v.x, this.y - v.y);
     }
     
-    
+    scalarMult(a : number) : Vec2d {
+        return new Vec2d(this.x * a, this.y * a);
+    }
+
+
     toString() : string {
         return "(" + this.x + ", " + this.y + ")";
     }
